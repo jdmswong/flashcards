c847db7f03364320d9a9cedf5f3d26f25e3cb4dd
JD Wong <jdmswong@gmail.com>
2014-08-18 20:53:12 -0700

    Began javascript development
    Page initializes with the front of the first of 3 hard coded flashcards.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
 	
-	console.log('asdf');
+	var flashCards = [
+		{front:"1 + 1 = _",		back:"2"},
+		{front:"a,b,c,d,_ ?",	back:"e"},
+		{front:"dat front",		back:"dat back"}
+	];
+	
+	var cardFrame = {
+		currentCard: flashCards[0],
+		element: $("#flashcard"),
+		showFront: function(){
+			$("#flashcard").text( this.currentCard.front );
+		}
+	};
+	
+	// Initializes board
+	(function initialize(){ 
+		refreshCounter(1);
+		
+		cardFrame.showFront();
+	})();
+	
+	// Sets counter to "x of <total>"
+	function refreshCounter(currentCount){
+		$(" #counter ").text(currentCount + " of " +flashCards.length);
+	}
+	
 });
