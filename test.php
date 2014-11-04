da47d12269f7cc72d0dbf761da875f06416603de
JD Wong <jdmswong@gmail.com>
2014-11-04 01:22:35 -0800

    test.php displays first 5 columns of flashcards.flashcards

--- test.php
+++ test.php
@@ -2,6 +2,28 @@
 
 echo "HELLO WORLD!<br>";
 
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>User ID</th><th>Card ID</th><th>Card Front</th><th>Card back</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
+
 $servername = "127.0.0.1";
 $username   = "jd";
 $password   = "Awesomemysql123";
@@ -9,10 +31,22 @@ $dbname     = "flashcards";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo $e -> getMessage();
-}
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM flashcards"); 
+    $stmt->execute();
 
-$conn = null; // close the connection
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+    $dsn = null;
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+$conn = null;
+echo "</table>";
 ?>
