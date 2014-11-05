404d2f04e42e462adcdc82dda6a0ab138b99b5a5
JD Wong <jdmswong@gmail.com>
2014-11-04 20:39:16 -0800

    new page flow, index->usecards.  Welcome page as main menu.

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title> Welcome to Flashcards!</title>
+    <link type="text/css" rel="stylesheet" href="css/reset.css"/>
+    <link type="text/css" rel="stylesheet" href="css/main.css"/>
+    <script src="js/jquery-2.0.3.js"></script>
+</head>
+<body>
+    Current options:<br>
+    <a href="addcards.php">Add cards</a><br>
+    <a href="usecards.php">Use cards</a><br>
+    
+</body>
