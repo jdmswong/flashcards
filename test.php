4c1b65b96118d1a5bf5013e7351e235bdffb6c5b
JD Wong <jdmswong@gmail.com>
2014-11-04 20:30:44 -0800

    test.php reads key value pairs from flashcardinput.txt

--- test.php
+++ test.php
@@ -2,30 +2,14 @@
 
 echo "HELLO WORLD!<br>";
 
+$myfile = fopen("flashcardinput.txt", "r") or die("Unable to open file!");
 
-$servername = "127.0.0.1";
-$username   = "jd";
-$password   = "Awesomemysql123";
-$dbname     = "flashcards";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT front,back FROM flashcards"); 
-    $stmt->execute();
-
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-
-    echo json_encode( $stmt->fetchAll() );
+while( !feof( $myfile) ){
+    echo fgets($myfile)."<br>\n";
     
-    
-    $dsn = null;
-    }
-catch(PDOException $e)
-    {
-    echo "Error: " . $e->getMessage();
-    }
-$conn = null;
-echo "</table>";
+}
+
+fclose($myfile);
+
+
 ?>
