08fec685c249e372a1009b23dbd039b378a9894f
JD Wong <jdmswong@gmail.com>
2014-11-12 02:31:09 -0800

    - can now choose manual or file upload box to appear, text input support not yet included - form validation checks for proper format

--- addcards_form.php
+++ addcards_form.php
@@ -13,17 +13,30 @@
 
     <?php require("header.php"); ?>
     
-    
-    Add flashcards:<br>
-    <form id="add-card-form" method="post" action="addcards.php" enctype="multipart/form-data">
-        Filename: <input type="file" name="input-file"/><br/>
-        
-        <?php $newDeckOption = true; require("inc/deckselect.inc"); ?>
-        <div id="new-deck-title">
-            New deck title: <input type="text" name="new-deck-title-input" />
-        </div>
-        <input type="submit" />
-    </form>
+    <div id="add-form-div">
+        Add flashcards:<br>
+        <form id="add-card-form" method="post" action="addcards.php" enctype="multipart/form-data">
+            <input type="radio" name="input-method" id="radio-upload" value="upload">File Upload</input>, 
+            <input type="radio" name="input-method" id="radio-manual" value="manual" checked="checked">Manual entry</input><br>
+            
+            <div id="file-upload">
+                Filename: <input type="file" name="input-file"/><br/>
+            </div>
+            
+
+            <textarea id="card-input" name="card-input" cols="40" rows="10">
+card 1 front, card 1 back
+card 2 front, card 2 back
+card 3 front, card 3 back
+( and so on... )</textarea>
+            
+            <?php $newDeckOption = true; require("inc/deckselect.inc"); ?>
+            <div id="new-deck-title">
+                New deck title: <input type="text" name="new-deck-title-input" />
+            </div>
+            <input type="submit" />
+        </form>
+    </div>
 
     <?php require("footer.php"); ?>
 
--- js/addcards_form.js
+++ js/addcards_form.js
@@ -3,23 +3,42 @@ $(document).ready(function(){
 // TODO check for duplicate deck name when creating new deck. call to php script perhaps?
     
 setDeckTitleDisplay();
+setMethodDisplay();
 $("select[name=deckid]").change( setDeckTitleDisplay );
+$("input[name=input-method]").change( setMethodDisplay );
+
+var cardInputClicked = false;
+$("#card-input").click( cardInputClick );
 
 // Force validation before submission
+var faultLine = "";
 $("#add-card-form").submit(function(){
-    console.log("submit function active");
     
     var errormsg = '';
-    if( $("select[name=deck-select]").val() == -1 &&
+    if( $("#deckid").val() == -1 &&
         $("input[name=new-deck-title-input]").val() == ""
     ){
         errormsg += "Provide a title for your new deck\n";
-        console.log("error msg 1 active");
     }
     
-    if( $("input[name=input-file]").val() == '' ){
-        errormsg += "Provide a filename";
-        console.log("error msg 2 active");
+    if( $("#radio-upload").prop("checked") == true &&
+        $("input[name=input-file]").val() == '' ){
+            
+        errormsg += "Provide a filename\n";
+        
+    }else if( $("#radio-manual").prop("checked") == true ){
+        if( 
+            cardInputClicked == false ||
+            $("#card-input").val().match(/^\s*$/) 
+        ){
+            errormsg += "Enter values in card input textbox\n";
+            
+        }else if( faultLine = validateCardInput() != true ){
+            
+            errormsg += "Line improperly formatted: "+faultLine+"\n";
+            
+        }
+        
     }
     
     if(errormsg != ''){
@@ -32,11 +51,44 @@ $("#add-card-form").submit(function(){
 
 function setDeckTitleDisplay(){
     if($("select[name=deckid]").val() != -1 ){
-        $("#new-deck-title").css("display","none");
+        $("#new-deck-title").hide();
     }else{
-        $("#new-deck-title").css("display","block");
+        $("#new-deck-title").show();
+    }
+}
+
+function setMethodDisplay(){
+    if(         $("#radio-upload").prop("checked") == true ){
+        $("#file-upload").show();
+        $("#card-input").hide();
+    }else if(   $("#radio-manual").prop("checked") == true ){
+        $("#file-upload").hide();
+        $("#card-input").show();
+    }
+}
+
+function cardInputClick(){
+    if(cardInputClicked == false){
+        cardInputClicked = true;
+        $("#card-input").text("");
     }
     
+}   
+
+// Ensures manually entered cards conform to "key,value"
+// returns true if validated, a string if not
+function validateCardInput(){
+    var lines = $("#card-input").val().split(/\n+/);
+    var i;
+    for(i=0; i < lines.length; i++){
+        if(!lines[i]){
+            continue;
+        }
+        if( !lines[i].match(/^\s*\S+\s*,\s*\s*\S+\s*$/) ){
+            return lines[i];
+        }
+    }
+    return true;
 }
-   
+
 });
