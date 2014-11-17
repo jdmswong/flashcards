89bbe9e4b6eafc6c913478478c2f993bffa5030d
JD Wong <jdmswong@gmail.com>
2014-11-17 00:03:53 -0800

    header greets you

--- header.php
+++ header.php
@@ -6,6 +6,11 @@
                 <div id="page-title">
                     <a href="index.php" >FlashCard Suite</a>
                 </div>
+                <div id="user-corner">
+                    <?php if(isset($_COOKIE['name'])) {
+                         echo '<p>Hello <span id="name">' . $_COOKIE['name'] . '</span>!</p>'; 
+                    } ?>
+                </div>
             </div>
                         
             <div id="content">
